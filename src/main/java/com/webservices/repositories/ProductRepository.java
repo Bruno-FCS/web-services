aa70a9da2d48bddcd2db6fceecfeb0019a71fc0b
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-06-06 11:42:06 -0300

    Product

---
+++ src/main/java/com/webservices/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.webservices.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.webservices.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
